df5ef8ccdefdbabc30f492bcfdf5e311a4fe086f
parth <patelparth3943@gmail.com>
2024-02-20 03:10:55 +0530

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,29 +6,19 @@ const About = () => {
       name="about"
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
-      <div className="max-w-screen-lg p-4  mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             About
           </p>
-          <p className="text-xl mt-20 text-justify">
-          Hello, I'm Parth Patel, a passionate web developer dedicated to crafting exceptional online experiences. With expertise in HTML, CSS,Javascript and React.js
-          My journey in web development has been marked by a commitment to transforming ideas into visually appealing and highly functional websites.
-          I bring a blend of creativity and technical expertise to every project.As a diligent problem solver, I enjoy tackling challenges head-on and finding elegant solutions that enhance the overall user experience.
-          Let's collaborate and turn your digital visions into reality –where every line of code tells a story of innovation and excellence.
-          Welcome to the world of Parth Patel, where coding meets creativity!
+        </div>
 
+        <p className="text-xl mt-20">
+        Hello, I'm Parth Patel, a passionate web developer dedicated to crafting exceptional online experiences. With expertise in HTML, CSS,Javascript and React.js My journey in web development has been marked by a commitment to transforming ideas into visually appealing and highly functional websites. I bring a blend of creativity and technical expertise to every project.As a diligent problem solver, I enjoy tackling challenges head-on and finding elegant solutions that enhance the overall user experience. Let's collaborate and turn your digital visions into reality –where every line of code tells a story of innovation and excellence. Welcome to the world of Parth Patel, where coding meets creativity!
 
         </p>
-        </div>
-     
-        
-        <div>
-          
-          <a href="/resume.pdf" className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer" download="resume.pdf">Resume</a>
-          
-        
-        </div>
+
+       
       </div>
     </div>
   );
