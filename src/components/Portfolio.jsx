f16c214736c4b37b5e158a0bcf6278377e1f8f68
parth <patelparth3943@gmail.com>
2024-02-19 15:05:34 +0530

    updated

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import industrial  from "../assets/portfolio/industrial.png";
+import ml from "../assets/portfolio/ml.png";
+import hms from "../assets/portfolio/hms.png";
+
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: industrial,
+    },
+    {
+      id: 2,
+      src: ml,
+    },
+    {
+      id: 3,
+      src: hms,
+    },
+    
+  ];
+
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md h-18  duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button className="w-1/2 px-6 py-3 m-4  duration-200 hover:scale-105">
+                  Demo
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
