fb96d2fe1594d8339c2a04542a6c8a9daa905b1b
parth <patelparth3943@gmail.com>
2024-06-14 23:14:53 +0530

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { Link } from "react-scroll";
+import { Link as ScrollLink } from "react-scroll";
 
 const NavBar = () => {
   const [nav, setNav] = useState(false);
@@ -20,11 +20,11 @@ const NavBar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <Link to="home" smooth duration={500}>
+        <ScrollLink to="home" smooth duration={500} offset={-20}>
           <h1 className="cursor-pointer text-5xl font-signature ml-2">
             Parth<span className="text-blue-500">.</span>
           </h1>
-        </Link>
+        </ScrollLink>
       </div>
 
       <ul className="hidden md:flex">
@@ -33,9 +33,9 @@ const NavBar = () => {
             key={id}
             className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:text-blue-400 hover:scale-105 duration-200"
           >
-            <Link to={link} smooth duration={500}>
+            <ScrollLink to={link} smooth duration={500} offset={-80}>
               {link}
-            </Link>
+            </ScrollLink>
           </li>
         ))}
       </ul>
@@ -48,20 +48,20 @@ const NavBar = () => {
       </div>
 
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-20 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+        <ul className="flex flex-col justify-start items-center absolute top-20 rounded-b-2xl right-0 w-80 h-100 bg-gradient-to-b from-black to-gray-800 text-gray-500">
           {links.map(({ id, link }) => (
             <li
               key={id}
               className="px-4 cursor-pointer capitalize py-6 text-4xl"
             >
-              <Link
+              <ScrollLink
                 onClick={toggleNav}
                 to={link}
                 smooth
                 duration={500}
               >
                 {link}
-              </Link>
+              </ScrollLink>
             </li>
           ))}
         </ul>
