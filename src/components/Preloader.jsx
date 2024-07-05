83d1fe41f12d32fbd9fb607b215136519fbec7b9
parth <patelparth3943@gmail.com>
2024-07-06 00:59:12 +0530

    loader  added

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -5,7 +5,9 @@ const Preloader = () => {
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gradient-to-b from-black via-black to-gray-800 z-50">
       <HashLoader
-  color="#583cd1"
+        color="#559ade"
+
+  className='border-white'
   size={100}
   speedMultiplier={1}
 />
